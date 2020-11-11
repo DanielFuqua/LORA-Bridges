593096d2b4352b964971e52816425dc7bf4f9014
robdan89 <fuquardwork@gmail.com>
2020-11-11 15:59:13 -0600

    nav lokoing good. got socials, items and logo. No actions yet.

--- components/Home/landing.js
+++ components/Home/landing.js
@@ -3,20 +3,19 @@ import Image from "next/image";
 
 const Landing = () => (
   <div className={styles.landing_section}>
-    {/* <div className={styles.hero_content}>
+    <div className={styles.hero_content}>
       <div className={styles.hero_title}>
-        <h1 className={styles.title}>L</h1>
+        <h1 className={styles.title_text}>L</h1>
         <img src="/logo.png" width="147px" height="133px" />
-        <h1 className={styles.title}>RA</h1>
+        <h1 className={styles.title_text}>RA</h1>
       </div>
-      <br />
       <p className={styles.sub_title}>Leadership Openness Results</p>
       <p className={styles.sub_title}>& Awareness</p>
         
       <div className={styles.hero_button}>
         <p className={styles.hero_button_text}>Start Building Bridges</p>
       </div>
-    </div> */}
+    </div>
   </div>
 );
 
--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,5 +1,60 @@
 import styles from "../../styles/nav.module.css";
+import Image from "next/image";
 
-const Nav = () => <div className={styles.nav}></div>;
+const Nav = () => (
+  <div className={styles.nav}>
+    <div className={styles.navLogo}>
+      <div className={styles.navLogo_text}>LORA</div>
+      <img src="/logo.png" width="95px" height="86px" />
+      <div className={styles.navLogo_text}>Bridges</div>
+    </div>
+
+    <div className={styles.social_icons}>
+      <div className={styles.icon}>
+        <a>
+          <Image src="/twitter.svg" alt="Twitter" width={17.9} height={18.85} />
+        </a>
+      </div>
+      <div className={styles.icon}>
+        <a>
+          <Image
+            src="/facebook.svg"
+            alt="Facebook"
+            width={22.08}
+            height={25.67}
+          />
+        </a>
+      </div>
+      <div className={styles.icon}>
+        <a>
+          <Image src="/insta.svg" alt="Insta" width={19} height={21.31} />
+        </a>
+      </div>
+    </div>
+
+    <ul className={styles.navItems}>
+      <li className={styles.li}>
+        <a className={styles.item} href="#">
+          About Us
+        </a>
+      </li>
+      <li className={styles.li}>
+        <a className={styles.item} href="#">
+          Services
+        </a>
+      </li>
+      <li className={styles.li}>
+        <a className={styles.item} href="#">
+          Resources
+        </a>
+      </li>
+      <li className={styles.li}>
+        <a className={styles.item} href="#">
+          Events
+        </a>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Nav;
