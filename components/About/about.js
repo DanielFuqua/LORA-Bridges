50cc4a6f086698c14d5237f416da9bb68fc7085e
robdan89 <fuquardwork@gmail.com>
2020-11-12 13:01:41 -0600

    about content on page - minus the LORA paragraph - but too much space between headshots and titles.

--- components/About/about.js
+++ components/About/about.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const About = () => (
   <div className={styles.about_section}>
-    <div>
+    <div className={styles.about_section_wrapper}>
       <div className={styles.about_header}>
         <Image
           src="/about_header.svg"
@@ -14,23 +14,56 @@ const About = () => (
         />
       </div>
 
-      {/* <div className={styles.about_content}>
-      <div className={styles.sally_section}>
-    
+      <div className={styles.about_content}>
+        <div className={styles.sally_section}>
+          <Image
+            src="/sally.svg"
+            alt="sally"
+            width={225}
+            height={300}
+            className={styles.sally}
+          />
+          <Image
+            src="/Sally Bartolameolli, MEd.svg"
+            alt="sally title"
+            width={225}
+            height={300}
+            className={styles.sally_title}
+          />
+        </div>
+        <div className={styles.mandala}>
+          <Image
+            src="/Mandala.svg"
+            alt="Mandala"
+            width={500}
+            height={500}
+            className={styles.manadala_pic}
+          />
+          <Image
+            src="/LORA Title.svg"
+            alt="Lora title"
+            width={170}
+            height={80}
+            className={styles.lora_title}
+          />
+        </div>
+        <div className={styles.kim_section}>
+          <Image
+            src="/Kim Siongco.svg"
+            alt="kim"
+            width={225}
+            height={300}
+            className={styles.kim}
+          />
+          <Image
+            src="/Kim Siongco.svg"
+            alt="kim title"
+            width={225}
+            height={300}
+            className={styles.kim_title}
+          />
+        </div>
       </div>
-      <div className={styles.mandala}>
-        <Image
-          src="/Mandala.svg"
-          alt="Mandala"
-          width={659}
-          height={659}
-          className={styles.manadala}
-        />
-      </div>
-      <div className={styles.kim_section}>
-      
-      </div>
-    </div> */}
     </div>
   </div>
 );
